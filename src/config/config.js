eed3d75b053c79b71bc2ceee3da077c6a4ab2a8d
snevadov <snevadov@gmail.com>
2019-06-27 23:44:27 -0500

    Corrección conexión a BD

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,13 @@
 process.env.PORT = process.env.PORT || 3000;
 
-process.env.URLDB = 'mongodb://localhost:27017/asignaturas';
+process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+
+let urlDB
+if (process.env.NODE_ENV === 'local'){
+	urlDB = 'mongodb://localhost:27017/asignaturas';
+}
+else {
+	urlDB = 'mongodb+srv://admin:12345@cursonodejstdea-3gfx5.mongodb.net/asignaturas?retryWrites=true&w=majority'
+}
+
+process.env.URLDB = urlDB
